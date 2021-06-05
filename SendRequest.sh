6c0643f16d5ae59d30864032123464a12f4533ed
OliverSkivdal <63880823+OliverSkivdal@users.noreply.github.com>
2021-06-05 15:14:18 +0200

    Adapting to change in API

--- SendRequest.sh
+++ SendRequest.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 curl -v -s --retry 5 -H "Accept: application/json" https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/2/query\?where\=1%3D1\&objectIds\=\&time\=\&geometry\=\&geometryType\=esriGeometryEnvelope\&inSR\=\&spatialRel\=esriSpatialRelIntersects\&resultType\=none\&distance\=0.0\&units\=esriSRUnit_Meter\&returnGeodetic\=false\&outFields\=\*\&returnGeometry\=false\&featureEncoding\=esriDefault\&multipatchOption\=xyFootprint\&maxAllowableOffset\=\&geometryPrecision\=\&outSR\=\&datumTransformation\=\&applyVCSProjection\=false\&returnIdsOnly\=false\&returnUniqueIdsOnly\=false\&returnCountOnly\=false\&returnExtentOnly\=false\&returnQueryGeometry\=false\&returnDistinctValues\=false\&cacheHint\=false\&orderByFields\=\&groupByFieldsForStatistics\=\&outStatistics\=\&having\=\&resultOffset\=\&resultRecordCount\=\&returnZ\=false\&returnM\=false\&returnExceededLimitFeatures\=true\&quantizationParameters\=\&sqlFormat\=none\&f\=pjson\&token\= > .reply.json
-sed -e '1,1783d' .reply.json > .result.json
+sed -e '1,1796d' .reply.json > .result.json
 sed '1,8!d' .result.json > .norway.json
 sed -e '2,4d' .norway.json > .filter1.json
 sed 's/,//g'  .filter1.json  > .filter2.json
